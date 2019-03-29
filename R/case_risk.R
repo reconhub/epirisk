32444c943351b1e3076dd4ade8502cf6a865f889
Thibaut Jombart <thibautjombart@gmail.com>
2019-03-29 14:51:32 +0200

    passing check; case_risk works

--- R/case_risk.R
+++ R/case_risk.R
@@ -7,21 +7,24 @@
 #' analysis. NOTE: this function is experimental and developed for the response
 #' to Ebola in North Kivu. Do not use it without consulting the authors first.
 #'
+#' 
 #' @details
 #'
-#' If $d(.)$ is the probability mass function of the delay from onset
+#' If \eqn{d(.)} is the probability mass function of the delay from onset
 #' of the index case to secondary infection, and $R$ the average reproduction
 #' number, the average risk of secondary infections is computed as:
 #'
-#' $$ risk(t) = R * \sum_{t = onset}^{isolation - 1} w(t) $$
+#' \deqn{ risk(t) = R * \sum_{t = onset}^{isolation - 1} w(t) }
+#' 
 #' 
 #' @export
+#' 
 #'
 #' @author Thibaut Jombart (thibautjombart@@gmail.com)
 #'
-#' @param onset: a vector of dates of onset
+#' @param onset a vector of dates of onset
 #'
-#' @param isolation: a vector of dates of isolation / outcome (when patients are
+#' @param isolation a vector of dates of isolation / outcome (when patients are
 #'   assumed to ' no longer seed infections)
 #'
 #' @param R the average reproduction number, i.e. number of secondary cases per
@@ -33,12 +36,34 @@
 #' @param reference_day the day to use as current date, as a `Date` object;
 #'   defaults to the current day
 #'
-#' @param dist_delay distribution of the delay from symptom onset to secondary
-#'   infections, as a `distcrete` object (see the RECON package of the same name
-#'   for more information)
+#' @param p_delay function giving the cumulative mass function of the delay
+#'   from symptom onset to secondary infections (in usual distributions,
+#'   starting with `p` e.g. `pgeom`)
+#'
+#' 
+#' @examples
+#'
+#' isolation <- today()  -  rpois(50, 10)
+#' onset <- isolation - rpois(50, 7)
+#' delay <- function(t) pgeom(t, prob = .2)
+#' risks <- case_risk(onset, isolation, R = 1.2,
+#'                    time_period = 14, p_delay = delay)
+#' risks
+
+#' df <- data.frame(id = factor(1:50), onset, isolation, risks)
+
+#' if (require(ggplot2)) {
+#'   ggplot(df, aes(y = id, yend = id)) +
+#'     geom_segment(aes(x = onset, xend = isolation, color = risks),
+#'                  size = 4) +
+#'     geom_vline(xintercept = today()) +
+#'     scale_color_gradientn("Risk 2nd cases", colors = c("#80aaff", "gold", "#b3003b")) +
+#'   labs(title = "Risk and infectious period",
+#'        x = "Infectious period (onset -> isolation)")
+#' }
 #' 
 
-case_risk <- function(onset, isolation, R, time_period, dist_delay,
+case_risk <- function(onset, isolation, R, time_period, p_delay,
                       reference_day = today()) {
   
   ## check that inputs are correct
@@ -52,7 +77,7 @@ case_risk <- function(onset, isolation, R, time_period, dist_delay,
                    class(isolation)[1])
     stop(msg)
   }
-  if (any(na.omit(onset > isolation))) {
+  if (any(stats::na.omit(onset > isolation))) {
     msg <- "Some dates of isolation predate the onset"
     stop(msg)
   }
@@ -84,14 +109,12 @@ case_risk <- function(onset, isolation, R, time_period, dist_delay,
     msg <- "`reference_day` needs to be a `Date` object"
   }
 
-  if (!inherits(dist_delay, "distcrete")) {
-    msg <- sprintf("`dist_delay` is not a `distcrete` object, but a %s",
-                   class(dist_delay)[1])
+  if (!inherits(p_delay, "function")) {
+    msg <- sprintf("`dist_delay` is not a `function`, but a %s",
+                   class(p_delay)[1])
     stop(msg)
   }
 
-  ## extract the cumulative mass function
-  p_delay <- dist_delay$p
 
   ## select cases to retain
   stop_at <- reference_day - time_period
@@ -100,7 +123,7 @@ case_risk <- function(onset, isolation, R, time_period, dist_delay,
     msg <- sprintf("No cases have an isolation date in the `time_period` %s - %s",
                    format(stop_at, "%d %b %Y"),
                    format(reference_day, "%d %b %Y"))
-    warning("msg")
+    warning(msg)
   }
 
 
@@ -111,7 +134,7 @@ case_risk <- function(onset, isolation, R, time_period, dist_delay,
   out <- p_delay(delays) * R
 
   ## set risks of cases outside the time window to zero
-  out[to_keep] <- 0
+  out[!to_keep] <- 0
   out
 
 }
--- R/today.R
+++ R/today.R
@@ -1,1 +1,7 @@
+#' Return current date
+#'
+#' This function returns the current date, as a `Date` object.
+#'
+#' @export
+#' 
 today <- function() as.Date(Sys.time())
